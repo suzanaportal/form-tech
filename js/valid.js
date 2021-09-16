1170e20ba685bcbdce955432f055dfd269d300c9
Sizana Portal <suzana.a.portal@gmail.com>
2021-09-15 21:25:58 -0300

    PRoblema com javascript referente a validação de dados resolvido, validando nome, e-mail, cep, apenas cpf com problemas.

--- js/valid.js
+++ js/valid.js
@@ -1,127 +1,32 @@
-let nome = document.getElementById("nome");
-  let email = document.getElementById("email");
-let cel = document.getElementById("cel");
-let cpf = document.getElementById("cpf");
-let cep = document.getElementById("cep");
-
 function validar() {
-if (nome.value == "") {
-  alert ("Por favor, preencha o campo nome.");
-  nome.focus();
-  return false;
-}
-
-  if(email.value = "" || email.indexOf("@") == -1 || email.indexOf("." == -1)) {
-    alert ("Por favor, preencha o campo e-mail.");
-    email.focus();
-    return false;
+  if (document.getElementById('nome').value == "" || document.getElementById('nome').value.length < 3 || document.getElementById('nome').value.length >= 50) {
+    alert("Preencha seu nome!");
+    document.getElementById('nome').focus();
+    return;
   }
-  if (cel.length != 11) {
-    alert ("Por favor, preencha o campo celular.")
-    cel.focus();
-    return false;
+
+  else if (document.getElementById('email').value == "" || document.getElementById('email').value.indexOf("@") == -1 || document.getElementById('email').value.indexOf(".") == -1) {
+    alert("Digite seu e-mail corretamente");
+    document.getElementById('email').focus();
+    return;
   }
-  if (cpf.value == "") {
-    alert ("Por favor, preencha o campo CPF.");
-    cpf.focus();
-    return false;
+
+  else if (document.getElementById('cel').value.length != 11) {
+    alert("Digite seu celular corretamente");
+    document.getElementById('cel').focus();
+    return;
   }
-  if (cep.value == "" ) {
-  alert ("Por favor, preencha o campo CEP.)";
-  cep.focus();
-  return false;
-}
+
+  else if (document.getElementById('cpf').value == "" || !testaCPF(document.getElementById('cpf').value)) {
+    alert("Digite seu cpf corretamente");
+    document.getElementById('cpf').focus();
+    return;
   }
-  
-function TestaCPF(CPF) {
-  var Soma;
-  var Resto;
-  Soma = 0;
-if (CPF == "00000000000") return false;
 
-for (i=1; i<=9; i++) Soma = Soma + parseInt(CPF.substring(i-1, i)) * (11 - i);
-Resto = (Soma * 10) % 11;
-
-  if ((Resto == 10) || (Resto == 11))  Resto = 0;
-  if (Resto != parseInt(CPF.substring(9, 10)) ) return false;
-
-Soma = 0;
-  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(CPF.substring(i-1, i)) * (12 - i);
-  Resto = (Soma * 10) % 11;
-
-  if ((Resto == 10) || (Resto == 11))  Resto = 0;
-  if (Resto != parseInt(CPF.substring(10, 11) ) ) return false;
-  return true;
-}
-var CPF = "12345678909";
-alert(TestaCPF(CPF));
-}
-}
-
-function limpa_formulário_cep() {
-  //Limpa valores do formulário de cep.
-  document.getElementById('rua').value=("");
-  document.getElementById('bairro').value=("");
-  document.getElementById('cidade').value=("");
-  document.getElementById('uf').value=("");
-  document.getElementById('ibge').value=("");
-}
-
-function meu_callback(conteudo) {
-if (!("erro" in conteudo)) {
-  //Atualiza os campos com os valores.
-  document.getElementById('rua').value=(conteudo.logradouro);
-  document.getElementById('bairro').value=(conteudo.bairro);
-  document.getElementById('cidade').value=(conteudo.localidade);
-  document.getElementById('uf').value=(conteudo.uf);
-  document.getElementById('ibge').value=(conteudo.ibge);
-} //end if.
-else {
-  //CEP não Encontrado.
-  limpa_formulário_cep();
-  alert("CEP não encontrado.");
-}
-}
-
-function pesquisacep(cep) {
-
-//Nova variável "cep" somente com dígitos.
-var cep = cep.replace(/\D/g, '');
-
-//Verifica se campo cep possui valor informado.
-if (cep != "") {
-
-  //Expressão regular para validar o CEP.
-  var validacep = /^[0-9]{8}$/;
-
-  //Valida o formato do CEP.
-  if(validacep.test(cep)) {
-
-      //Preenche os campos com "..." enquanto consulta webservice.
-      document.getElementById('rua').value="...";
-      document.getElementById('bairro').value="...";
-      document.getElementById('cidade').value="...";
-      document.getElementById('uf').value="...";
-      document.getElementById('ibge').value="...";
-
-      //Cria um elemento javascript.
-      var script = document.createElement('script');
-
-      //Sincroniza com o callback.
-      script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-      //Insere script no documento e carrega o conteúdo.
-      document.body.appendChild(script);
-
-  } //end if.
-  else {
-      //cep é inválido.
-      limpa_formulário_cep();
-      alert("Formato de CEP inválido.");
+  else if (document.getElementById('cep').value == "" || !pesquisacep(document.getElementById('cep').value)) {
+    alert("Digite seu cep");
+    document.getElementById('cep').focus();
+    return;
   }
-} //end if.
-else {
-  //cep sem valor, limpa formulário.
-  limpa_formulário_cep();
+
 }
-};
---
+++ js/validaDados.js
@@ -0,0 +1,88 @@
+function testaCPF(strCPF) {
+  var Soma;
+  var Resto;
+  Soma = 0;
+if (strCPF == "00000000000") return false;
+
+for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11))  Resto = 0;
+  if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+Soma = 0;
+  for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+  Resto = (Soma * 10) % 11;
+
+  if ((Resto == 10) || (Resto == 11))  Resto = 0;
+  if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+  return true;
+}
+
+function limpa_formulário_cep() {
+  //Limpa valores do formulário de cep.
+  document.getElementById('rua').value=("");
+  document.getElementById('bairro').value=("");
+  document.getElementById('cidade').value=("");
+  document.getElementById('uf').value=("");
+  document.getElementById('ibge').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+  //Atualiza os campos com os valores.
+  document.getElementById('rua').value=(conteudo.logradouro);
+  document.getElementById('bairro').value=(conteudo.bairro);
+  document.getElementById('cidade').value=(conteudo.localidade);
+  document.getElementById('uf').value=(conteudo.uf);
+  document.getElementById('ibge').value=(conteudo.ibge);
+} //end if.
+else {
+  //CEP não Encontrado.
+  limpa_formulário_cep();
+  alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+  //Expressão regular para validar o CEP.
+  var validacep = /^[0-9]{8}$/;
+
+  //Valida o formato do CEP.
+  if(validacep.test(cep)) {
+
+      //Preenche os campos com "..." enquanto consulta webservice.
+      document.getElementById('rua').value="...";
+      document.getElementById('bairro').value="...";
+      document.getElementById('cidade').value="...";
+      document.getElementById('uf').value="...";
+      document.getElementById('ibge').value="...";
+
+      //Cria um elemento javascript.
+      var script = document.createElement('script');
+
+      //Sincroniza com o callback.
+      script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+      //Insere script no documento e carrega o conteúdo.
+      document.body.appendChild(script);
+
+  } //end if.
+  else {
+      //cep é inválido.
+      limpa_formulário_cep();
+      alert("Formato de CEP inválido.");
+  }
+} //end if.
+else {
+  //cep sem valor, limpa formulário.
+  limpa_formulário_cep();
+}
+};
